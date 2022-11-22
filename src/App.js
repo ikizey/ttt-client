20762b34624ad4a9a5fd84cacbd1a2a467e93d5b
kizey <ikizey@icloud.com>
2022-11-23 02:13:18 +0400

    install react-toastify

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Welcome from './pages/Welcome';
 import Game from './pages/Game';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/'>
-        <Route index element={<Welcome />} />
-        <Route path='/game' element={<Game />} />
-      </Route>
-    </Routes>
+    <React.Fragment>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Welcome />} />
+          <Route path='/game' element={<Game />} />
+        </Route>
+      </Routes>
+      <ToastContainer />
+    </React.Fragment>
   );
 }
 
---
+++ src/toastConfig.js
@@ -0,0 +1,10 @@
+export const toastConfig = {
+  position: 'top-center',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: false,
+  draggable: false,
+  progress: undefined,
+  theme: 'colored',
+};

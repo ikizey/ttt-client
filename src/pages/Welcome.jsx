c3e8c8e5abbc3c713ba732101e0d95165de6ddf9
kizey <ikizey@icloud.com>
2022-11-27 19:50:50 +0400

    save name to local storage

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,37 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { GameContext } from '../store/GameContext';
 
 const Welcome = () => {
-  const { askForRoom, playerName, setPlayerName, playerID, totalPlayers } =
+  const { askForRoom, playerName, setPlayerName, totalPlayers } =
     useContext(GameContext);
 
-  const nameChangeHandle = (event) => {
-    event.preventDefault();
-    setPlayerName(event.currentTarget.userName.value);
+  useEffect(() => {
+    setPlayerName(localStorage.getItem('playerName') || '');
+  }, [setPlayerName]);
+
+  const saveName = (name) => {
+    if (name) {
+      setPlayerName(name);
+      localStorage.setItem('playerName', name);
+    }
   };
 
   return (
     <div className='container flex flex-col justify-center items-center gap-y-4'>
-      <div>
-        Welcome to TicTacToe {playerID} {playerName}
-      </div>
-      <form onSubmit={nameChangeHandle}>
-        <input
-          className='px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-orange-600'
-          type='text'
-          name='userName'
-          id='userName'
-          placeholder='Enter name'
-        />
-        <button className='hidden' type='submit'>
-          Change name
-        </button>
-      </form>
+      <div>Welcome to TicTacToe</div>
+      <input
+        className='px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-orange-600'
+        type='text'
+        name='userName'
+        id='userName'
+        placeholder='Enter name'
+        value={playerName}
+        onChange={(e) => saveName(e.target.value)}
+      />
+      <button className='hidden' type='submit'>
+        Change name
+      </button>
       <Link
         to='/game'
         className='px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900'
-        onClick={askForRoom}
+        onClick={() => (playerName ? askForRoom : null)}
       >
         Find random opponent
       </Link>
